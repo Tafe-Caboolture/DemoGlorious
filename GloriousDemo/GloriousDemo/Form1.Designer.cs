3c75101ed70f16e2936f3f73a854e7d891d77978
Matt-The-Programmer <mattmilzewski@gmail.com>
2018-02-06 11:55:09 +1000

    V3

--- GloriousDemo/GloriousDemo/Form1.Designer.cs
+++ GloriousDemo/GloriousDemo/Form1.Designer.cs
@@ -29,10 +29,7 @@
         private void InitializeComponent()
         {
             this.label1 = new System.Windows.Forms.Label();
-            this.label2 = new System.Windows.Forms.Label();
             this.lblmatty = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
-            this.label4 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -40,57 +37,24 @@
             this.label1.AutoSize = true;
             this.label1.Location = new System.Drawing.Point(96, 90);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(77, 13);
+            this.label1.Size = new System.Drawing.Size(165, 13);
             this.label1.TabIndex = 0;
-            this.label1.Text = "Keenan Sucks";
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(176, 90);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(29, 13);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Balls";
-            this.label2.Click += new System.EventHandler(this.label2_Click);
+            this.label1.Text = "We are a super cooperative team";
             // 
             // lblmatty
             // 
             this.lblmatty.AutoSize = true;
             this.lblmatty.Location = new System.Drawing.Point(96, 122);
             this.lblmatty.Name = "lblmatty";
-            this.lblmatty.Size = new System.Drawing.Size(141, 13);
+            this.lblmatty.Size = new System.Drawing.Size(0, 13);
             this.lblmatty.TabIndex = 2;
-            this.lblmatty.Text = "matt takes it down the throat";
-            // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(96, 160);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(86, 13);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Doms a shit cunt";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(12, 161);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(480, 13);
-            this.label4.TabIndex = 3;
-            this.label4.Text = "Does anyone else think it\'s funny dom has changed this but not got rid of the fac" +
-    "t he also sucks balls";
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(497, 261);
-            this.Controls.Add(this.label4);
-            this.Controls.Add(this.label3);
             this.Controls.Add(this.lblmatty);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "Form1";
@@ -102,10 +66,7 @@
         #endregion
 
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label lblmatty;
-        private System.Windows.Forms.Label label3;
-        private System.Windows.Forms.Label label4;
     }
 }
 
