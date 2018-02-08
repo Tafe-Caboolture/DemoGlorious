6984f08d0fd8ffc020c5972546cd4547da9ad0c4
novaking27 <elianderson@me.com>
2018-02-09 09:35:39 +1000

    Just added a lable

--- GloriousDemo/GloriousDemo/Form1.Designer.cs
+++ GloriousDemo/GloriousDemo/Form1.Designer.cs
@@ -34,63 +34,80 @@
             this.label2 = new System.Windows.Forms.Label();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
             this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(96, 90);
+            this.label1.Location = new System.Drawing.Point(144, 138);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(165, 13);
+            this.label1.Size = new System.Drawing.Size(243, 20);
             this.label1.TabIndex = 0;
             this.label1.Text = "We are a super cooperative team";
             // 
             // lblmatty
             // 
             this.lblmatty.AutoSize = true;
-            this.lblmatty.Location = new System.Drawing.Point(96, 122);
+            this.lblmatty.Location = new System.Drawing.Point(144, 188);
+            this.lblmatty.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.lblmatty.Name = "lblmatty";
-            this.lblmatty.Size = new System.Drawing.Size(0, 13);
+            this.lblmatty.Size = new System.Drawing.Size(0, 20);
             this.lblmatty.TabIndex = 2;
             // 
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(99, 154);
+            this.label2.Location = new System.Drawing.Point(148, 237);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(93, 13);
+            this.label2.Size = new System.Drawing.Size(136, 20);
             this.label2.TabIndex = 3;
             this.label2.Text = "The A team collab";
             // 
             // pictureBox1
             // 
             this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
-            this.pictureBox1.Location = new System.Drawing.Point(267, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(400, 18);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(218, 212);
+            this.pictureBox1.Size = new System.Drawing.Size(327, 326);
             this.pictureBox1.TabIndex = 4;
             this.pictureBox1.TabStop = false;
             // 
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(320, 227);
+            this.label3.Location = new System.Drawing.Point(480, 349);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(85, 13);
+            this.label3.Size = new System.Drawing.Size(126, 20);
             this.label3.TabIndex = 5;
             this.label3.Text = "Its a gif Run app";
             // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(169, 328);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(102, 20);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Just find this ";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(497, 261);
+            this.ClientSize = new System.Drawing.Size(746, 402);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.label3);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.lblmatty);
             this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Form1";
             this.Text = "Form1";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
@@ -106,6 +123,7 @@
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
     }
 }
 
